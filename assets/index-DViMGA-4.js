af97faaa47ef55dea81b5e9569e5b0d8673f11b6
Kai <bailongkai@gmail.com>
2026-09-11 18:45:30 -0700

    Deploy: animation

--- assets/index-DViMGA-4.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-CL916OPj.js","./index-i7p6xOSV.js","./phaser-D3izK-R_.js"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-i7p6xOSV.js";import"./phaser-D3izK-R_.js";const _=r("Preferences",{web:()=>t(()=>import("./web-CL916OPj.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.PreferencesWeb)});export{_ as Preferences};
--- assets/index-DnZueZLi.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-DQJ9ogc7.js","./index-i7p6xOSV.js","./phaser-D3izK-R_.js"])))=>i.map(i=>d[i]);
-import{r as e,_ as p}from"./index-i7p6xOSV.js";import"./phaser-D3izK-R_.js";const i=e("App",{web:()=>p(()=>import("./web-DQJ9ogc7.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(r=>new r.AppWeb)});export{i as App};
