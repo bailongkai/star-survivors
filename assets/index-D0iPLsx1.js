a73d25b89a48af8627969fcd2cf702c67181d8c2
Kai <bailongkai@gmail.com>
2026-09-11 17:14:22 -0700

    Deploy: custom art

--- assets/index-D0iPLsx1.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-Bj7np7iO.js","./index-DdlceM-R.js","./phaser-D3izK-R_.js"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-DdlceM-R.js";import"./phaser-D3izK-R_.js";const _=r("Preferences",{web:()=>t(()=>import("./web-Bj7np7iO.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.PreferencesWeb)});export{_ as Preferences};
--- assets/index-DRVWaKk4.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-CWK2vySD.js","./index-DdlceM-R.js","./phaser-D3izK-R_.js"])))=>i.map(i=>d[i]);
-import{r as e,_ as p}from"./index-DdlceM-R.js";import"./phaser-D3izK-R_.js";const i=e("App",{web:()=>p(()=>import("./web-CWK2vySD.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(r=>new r.AppWeb)});export{i as App};
