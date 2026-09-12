9592c3219c92665473b5310371fccb694c6954b9
Kai <bailongkai@gmail.com>
2026-09-12 16:32:26 -0700

    Deploy: three new characters

--- assets/index-B75O7SkL.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-BieCmrGI.js","./index-Bxw8j_tj.js","./phaser-D3izK-R_.js"])))=>i.map(i=>d[i]);
-import{r,_ as t}from"./index-Bxw8j_tj.js";import"./phaser-D3izK-R_.js";const _=r("Preferences",{web:()=>t(()=>import("./web-BieCmrGI.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(e=>new e.PreferencesWeb)});export{_ as Preferences};
