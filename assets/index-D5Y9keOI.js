b9a57bcc9c4aaf30e696df4ac5b14a3250ad50f1
Kai <bailongkai@gmail.com>
2026-09-13 00:41:56 -0700

    Deploy: telemetry to PostHog

--- assets/index-D5Y9keOI.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-B1j1BGpR.js","./index-jv4Og1li.js","./phaser-D3izK-R_.js","./module-BgzLZHoS.js"])))=>i.map(i=>d[i]);
-import{r as t,_ as r}from"./index-jv4Og1li.js";import"./phaser-D3izK-R_.js";const _=t("Posthog",{web:()=>r(()=>import("./web-B1j1BGpR.js"),__vite__mapDeps([0,1,2,3]),import.meta.url).then(o=>new o.PosthogWeb)});export{_ as Posthog};
---
+++ assets/index-h68-ry__.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-iN6cAYpW.js","./index-BFfE4Pmj.js","./phaser-D3izK-R_.js"])))=>i.map(i=>d[i]);
+import{r as e,_ as p}from"./index-BFfE4Pmj.js";import"./phaser-D3izK-R_.js";const i=e("App",{web:()=>p(()=>import("./web-iN6cAYpW.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(r=>new r.AppWeb)});export{i as App};
