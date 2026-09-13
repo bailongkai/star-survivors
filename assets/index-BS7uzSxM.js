2c0a01445e51ab2bbf575122a57ee7174fda01b4
Kai <bailongkai@gmail.com>
2026-09-12 21:15:33 -0700

    Deploy: telemetry

--- assets/index-BS7uzSxM.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-CL_bY9wJ.js","./index-C_7Xpx__.js","./phaser-D3izK-R_.js"])))=>i.map(i=>d[i]);
-import{r as e,_ as p}from"./index-C_7Xpx__.js";import"./phaser-D3izK-R_.js";const i=e("App",{web:()=>p(()=>import("./web-CL_bY9wJ.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(r=>new r.AppWeb)});export{i as App};
---
+++ assets/index-DUAvZpLX.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-BfbbHZN4.js","./index-jv4Og1li.js","./phaser-D3izK-R_.js"])))=>i.map(i=>d[i]);
+import{r as e,_ as p}from"./index-jv4Og1li.js";import"./phaser-D3izK-R_.js";const i=e("App",{web:()=>p(()=>import("./web-BfbbHZN4.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(r=>new r.AppWeb)});export{i as App};
